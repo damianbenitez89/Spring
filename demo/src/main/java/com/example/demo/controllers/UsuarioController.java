3e0e9655d4711a1717b5e8bce5e994a04ce59835
damianbenitez89 <78812666+damianbenitez89@users.noreply.github.com>
2021-09-22 22:48:13 -0300

    class

--- demo/src/main/java/com/example/demo/controllers/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -1,13 +1,55 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setApellido("benitez");
+        usuario.setNombre("Damian");
+        usuario.setEmail("benitez@gmail.com");
+        usuario.setTelefono("1141764794");
+        usuario.setPassword("35081213a");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario1")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setApellido("benitez");
+        usuario.setNombre("Damian");
+        usuario.setEmail("benitez@gmail.com");
+        usuario.setTelefono("1141764794");
+        usuario.setPassword("35081213a");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario2")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setApellido("benitez");
+        usuario.setNombre("Damian");
+        usuario.setEmail("benitez@gmail.com");
+        usuario.setTelefono("1141764794");
+        usuario.setPassword("35081213a");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario3")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setApellido("benitez");
+        usuario.setNombre("Damian");
+        usuario.setEmail("benitez@gmail.com");
+        usuario.setTelefono("1141764794");
+        usuario.setPassword("35081213a");
+        return usuario;
     }
 }
