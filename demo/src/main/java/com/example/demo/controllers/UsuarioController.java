6e3940a3320023bac1e8fec90442e9394cac4408
damianbenitez89 <78812666+damianbenitez89@users.noreply.github.com>
2021-11-29 13:14:41 -0300

    datos que no se utilizan

--- demo/src/main/java/com/example/demo/controllers/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -22,22 +22,8 @@ public class UsuarioController {
     @Autowired // hace que UsuarioDaoImplemente cree un objeto y la guarde en la variable usario Dao
     private UsuarioDao usuarioDao;
 
-
     @Autowired
     private JWTUtil jwtUtil;
-
-    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET) // metodo HET para traer  un usuario
-    public Usuario getUsuario(@PathVariable long id){
-        Usuario usuario = new Usuario();
-        usuario.setId(id);
-        usuario.setApellido("benitez");
-        usuario.setNombre("Damian");
-        usuario.setEmail("benitez@gmail.com");
-        usuario.setTelefono("1141764794");
-        usuario.setPassword("35081213a");
-        return usuario;
-    }
-
 
     @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)// INDICA EN LA URL DEL CONTENIDO EN ESTE CASO LOCALHOST:8080/USUARIOS
     public List<Usuario> getUsuarios(@RequestHeader(value="Authorization") String token){ // como parametro le vamos a pasar la autorizacion y lo vamos poner en una variable token
@@ -46,47 +32,21 @@ public class UsuarioController {
                 if(usuarioId==null){
                     return new ArrayList<>();
                 }
-
         return usuarioDao.getUsuarios();
-
     }
 
     @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)// metodo POST para insertar  en base de datos
-    public void registrarUsuario(@RequestBody Usuario usuario){ // con esto el JSON que recibe lo convierte en usuario automaticamente
+    public void registrarUsuario(@RequestBody Usuario usuario){ // con la anotacion esto el JSON que recibe lo convierte en usuario automaticamente
 
         Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id); //creacion de variable para generar el hasheo
         String hash = argon2.hash(1,1024,1,usuario.getPassword()); // el dato que dese hashear, iteraciones, memoria , paralellismo y password
         usuario.setPassword(hash); // aca enviamos la contraceña hasheada
         usuarioDao.registrar(usuario);
-
-    }
-
-    @RequestMapping(value = "usuario1")
-    public Usuario editar(){
-        Usuario usuario = new Usuario();
-        usuario.setApellido("benitez");
-        usuario.setNombre("Damian");
-        usuario.setEmail("benitez@gmail.com");
-        usuario.setTelefono("1141764794");
-        usuario.setPassword("35081213a");
-        return usuario;
     }
 
     @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
     public void eliminar(@PathVariable long id ){ //no va a devolver nada
         usuarioDao.eliminar(id);
     }
-
-    @RequestMapping(value = "usuario3")
-    public Usuario buscar(){
-        Usuario usuario = new Usuario();
-        usuario.setApellido("benitez");
-        usuario.setNombre("Damian");
-        usuario.setEmail("benitez@gmail.com");
-        usuario.setTelefono("1141764794");
-        usuario.setPassword("35081213a");
-        return usuario;
-    }
-
 
 }
